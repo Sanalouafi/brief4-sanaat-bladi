afe615c498cf629be0151407bd578d33577bb8fa
Sanalouaf <sanalouafi2003@gmail.com>
2023-11-02 15:11:29 +0100

    add background color to dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -79,3 +79,30 @@ var myLineChart = new Chart(ctxL, {
     responsive: true
   }
 });
+
+
+
+
+//////////////////////
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Sidebar Toggler
+    $('.sidebar-toggler').click(function () {
+        $('.sidebar, .content').toggleClass("open");
+        return false;
+    });
+
+
+    // Progress Bar
+    $('.pg-bar').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
