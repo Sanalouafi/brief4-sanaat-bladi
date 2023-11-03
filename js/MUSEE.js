03c6ba38e246393be1fd1d3595712d926682c77c
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2023-11-03 16:28:12 +0100

    version final de page musee

---
+++ js/MUSEE.js
@@ -0,0 +1,17 @@
+let forml= document.querySelector(".form-email")
+let form_input=document.querySelector("#email_input")
+let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+console.log(form_input)
+
+forml.addEventListener("submit",function(u) {
+   u.preventDefault()
+   console.log(form_input.value)
+   if (form_input.value ==="" || !form_input.value.match(emailRegex) ) {
+    form_input.nextElementSibling.style.display="block"; 
+   }
+   else{
+    form_input.nextElementSibling.style.display="none"; 
+   }
+})
